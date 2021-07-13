6237fdf6d2dbf36f2d5521f85f2b49a0c14e9fea
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2021-07-13 19:43:49 +0800

    update sidebar
    增加侧边栏样式

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sideBarContent">
-    <div style="margin:0.7rem 0;background:none;height:2.5rem;border:0px solid black" v-for="i in menu" :key="i.name">
-      <button style="width:100%;border:none;background:#7dbcff;height:2.5rem">{{i.name}}</button>
+    <div class="btnBox" v-for="(i, index) in menu" :key="i.id">
+      <button class="btn" :class="{show: selectBtn == index}"  @click="selectBtn = index">{{i.name}}</button>
     </div>
   </div>
 </template>
@@ -11,44 +11,55 @@ export default {
   name: 'sideBar',
   data () {
     return {
-      role: 2
+      role: 0,
+      selectBtn: 0
     }
   },
   computed: {
     menu () {
       let admin = [
         {
+          id: 1,
           name: '公告信息'
           // path: '/notice'
         },
         {
+          id: 2,
           name: '宿舍管理员信息'
           // path: '/notice'
         },
         {
+          id: 3,
           name: '考勤任务'
           // path: '/notice'
         }
       ]
       let houseparent = [
         {
+          id: 1,
           name: '公告信息'
           // path
         },
         {
+          id: 2,
           name: '个人信息'
         }
       ]
       let student = [
         {
+          id: 1,
           name: '个人信息'
         },
         {
+          id: 2,
           name: '个人信息'
         }
       ]
       return this.role === 0 ? admin : this.role === 1 ? houseparent : student
     }
+  },
+  watch: {
+
   }
 
 }
@@ -65,5 +76,22 @@ export default {
   width: 15%;
   float: left;
   background: #b3d8ff;
+  .btnBox{
+    margin:0.7rem 0;
+    background:none;
+    height:2.5rem;
+    border:0px solid black;
+    .btn{
+      cursor: pointer;
+      width:100%;
+      border:none;
+      // background:#7dbcff;
+      background:#abcef3;
+      height:2.5rem;
+    }
+    .show{
+       background:#7dbcff;
+    }
+  }
 }
 </style>
